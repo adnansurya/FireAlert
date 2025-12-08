8603176a06ce2fd82868a3fd842c6b73b9fa161c
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-12-08 18:01:08 +0800

    renew files

--- app/src/main/java/com/example/firealert/HomeActivity.java
+++ app/src/main/java/com/example/firealert/HomeActivity.java
@@ -18,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-
 import android.content.pm.PackageManager;
 
 import com.google.firebase.database.DataSnapshot;
@@ -30,219 +29,157 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView tvDateTime, tvFlame, tvLpg, tvSuhu, tvStatus;
+    private ImageView imgStatusIcon;
+
     private Handler handler = new Handler();
     private Runnable updateTimeRunnable;
 
     private static final String CHANNEL_ID = "my_channel_id";
     private static final String CHANNEL_NAME = "my_channel_name";
 
-
     boolean fireAlarm = false;
-
     boolean lpgAlarm = false;
-    
     boolean suhuAlarm = false;
 
     String currentDateTime;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Inisialisasi TextView untuk waktu real-time dan sensor
         tvDateTime = findViewById(R.id.tvDateTime);
         tvFlame = findViewById(R.id.tvApiStatus);
         tvLpg = findViewById(R.id.tvGasStatus);
         tvSuhu = findViewById(R.id.tvTemperature);
         tvStatus = findViewById(R.id.tvStatus);
+        imgStatusIcon = findViewById(R.id.imgStatusIcon);
 
-        // Update waktu secara real-time
-        updateTimeRunnable = new Runnable() {
-            @Override
-            public void run() {
-                currentDateTime = new SimpleDateFormat("EEEE, dd MMMM yyyy\nhh:mm:ss a", Locale.getDefault()).format(new Date());
-                tvDateTime.setText(currentDateTime);
-//                Notifikasi(HomeActivity.this,"Terjadi Kebakaran", "Api Terdeteksi", "flame");
-                handler.postDelayed(this, 1000); // Perbarui setiap detik
-            }
+        updateTimeRunnable = () -> {
+            currentDateTime = new SimpleDateFormat("EEEE, dd MMMM yyyy\nhh:mm:ss a",
+                    Locale.getDefault()).format(new Date());
+            tvDateTime.setText(currentDateTime);
+            handler.postDelayed(updateTimeRunnable, 1000);
         };
         handler.post(updateTimeRunnable);
 
-        // Ambil data sensor dari Firebase
         fetchSensorData();
 
-
-
-        // Tombol Riwayat Notifikasi
         Button btnNotificationHistory = findViewById(R.id.btnNotificationHistory);
-        btnNotificationHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Pindah ke aktivitas riwayat notifikasi
-                Intent intent = new Intent(HomeActivity.this, NotificationHistoryActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Ikon Logout
-        ImageView logoutIcon = findViewById(R.id.logoutIcon);
-        logoutIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Pindah ke aktivitas utama (MainActivity)
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish(); // Menutup aktivitas saat ini
-            }
+        btnNotificationHistory.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, NotificationHistoryActivity.class);
+            startActivity(intent);
         });
     }
 
-    // Fungsi untuk mengambil data sensor dari Firebase
+    // ==============================
+    // AMBIL DATA SENSOR DARI FIREBASE
+    // ==============================
     private void fetchSensorData() {
-        DatabaseReference sensorsRef = FirebaseDatabase.getInstance().getReference("sensors").child("-OEh1BMjMp-WvVPltf7r");
+        DatabaseReference sensorsRef = FirebaseDatabase.getInstance()
+                .getReference("sensors")
+                .child("-OEnVfTt23-cM71d8RJd");
 
         sensorsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    // Ambil nilai sensor dari Firebase
+
                     int flame = dataSnapshot.child("flame").getValue(Integer.class);
                     double lpg = dataSnapshot.child("lpg").getValue(Double.class);
                     double suhu = dataSnapshot.child("suhu").getValue(Double.class);
 
-                    if(flame == 1){
-
-                        if(fireAlarm == false){
-                            Notifikasi(HomeActivity.this,"Terjadi Kebakaran!", "Api Terdeteksi", "flame");
+                    // FLAME
+                    if (flame == 1) {
+                        if (!fireAlarm) {
+                            Notifikasi(HomeActivity.this, "Terjadi Kebakaran!", "Api Terdeteksi", "flame");
                             writeNotif("Api Terdeteksi", flame, lpg, suhu);
                         }
                         fireAlarm = true;
-                        Log.d("FLAME", "Api");
-//
+                    } else fireAlarm = false;
 
-                    }else{
-                        fireAlarm = false;
-                    }
-                    if(lpg >= 50.0){
-                        Log.d("LPG", "Gas");
-
-                        if(lpgAlarm == false){
-                            Notifikasi(HomeActivity.this,"Terjadi Kebakaran!", "Kadar Gas Melebihi Batas", "lpg");
+                    // GAS
+                    if (lpg >= 50.0) {
+                        if (!lpgAlarm) {
+                            Notifikasi(HomeActivity.this, "Terjadi Kebakaran!", "Kadar Gas Melebihi Batas", "lpg");
                             writeNotif("Kadar Gas Melebihi Batas", flame, lpg, suhu);
                         }
                         lpgAlarm = true;
+                    } else lpgAlarm = false;
 
-                    }else{
-                        lpgAlarm = false;
-                    }
-
-                    if(suhu >= 42.0){
-                        Log.d("SUHU", "Suhu");
-                        if(suhuAlarm == false){
+                    // SUHU
+                    if (suhu >= 42.0) {
+                        if (!suhuAlarm) {
                             Notifikasi(HomeActivity.this, "Terjadi Kebakaran!", "Suhu Melebihi Batas", "suhu");
                             writeNotif("Suhu Melebihi Batas", flame, lpg, suhu);
                         }
                         suhuAlarm = true;
+                    } else suhuAlarm = false;
 
-                    }else{
-                        suhuAlarm = false;
-                    }
-
-                    if(!fireAlarm && !suhuAlarm && !lpgAlarm){
+                    // ==============================
+                    // UBAH STATUS + GANTI IKON
+                    // ==============================
+                    if (!fireAlarm && !suhuAlarm && !lpgAlarm) {
                         tvStatus.setText("Kondisi Aman");
-                    }else{
+                        tvStatus.setTextColor(Color.parseColor("#008F39"));
+                        imgStatusIcon.setImageResource(R.drawable.check_green);
+                    } else {
                         tvStatus.setText("Terjadi Kebakaran!");
+                        tvStatus.setTextColor(Color.parseColor("#BD1111"));
+                        imgStatusIcon.setImageResource(R.drawable.warning); // ⛔ WARNING ICON
                     }
 
-
-                    if(flame == 1){
-                        tvFlame.setText("Api Terdeteksi" );
-                    }else{
-                        tvFlame.setText("Tidak Ada Api" );
-                    }
-
-                    // Set nilai ke TextView
-
+                    // Update sekunder
+                    tvFlame.setText(flame == 1 ? "Api Terdeteksi" : "Tidak Ada Api");
                     tvLpg.setText("LPG: " + lpg);
                     tvSuhu.setText("Suhu: " + suhu + " °C");
-
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Jika terjadi error
-                tvFlame.setText("Flame: Error");
-                tvLpg.setText("LPG: Error");
-                tvSuhu.setText("Suhu: Error");
+
             }
         });
     }
 
-
+    // ==============================
+    // NOTIFIKASI
+    // ==============================
     public void Notifikasi(Context context, String title, String content, String kondisi) {
 
-
-
-        // Tampilkan notifikasi
-        // Membuat notifikasi baru
-        Notif newNotification = new Notif(title, content);
-        newNotification.setMessage(content);
-        newNotification.setId(System.currentTimeMillis());  // Simpan waktu sekarang
-
-
-
-        // Tampilkan notifikasi
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle(title) // Gunakan kondisi sebagai judul notifikasi
+                .setContentTitle(title)
                 .setContentText(content)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
-            channel.enableLights(true);
-            channel.setLightColor(Color.RED);
-            channel.enableVibration(true);
-            builder.setChannelId(CHANNEL_ID);
+            NotificationChannel channel =
+                    new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
             notificationManager.createNotificationChannel(channel);
-
+            builder.setChannelId(CHANNEL_ID);
         }
 
-        // Tampilkan notifikasi
-        if (ActivityCompat.checkSelfPermission(HomeActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Handle permission
-            return;
-        }
-        Log.d("NOTIF", "ON");
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) return;
+
         notificationManager.notify(getNotificationId(kondisi), builder.build());
-
-        // Simpan waktu terakhir notifikasi ditampilkan ke Firebase Database
-
-
     }
-
 
     private int getNotificationId(String kondisi) {
         switch (kondisi) {
-            case "suhu":
-                return 1;
-            case "flame":
-                return 2;
-            case "lpg":
-                return 3;
-            default:
-                return 0; // Atur sesuai kebutuhan, pastikan semua ID notifikasi berbeda
+            case "suhu": return 1;
+            case "flame": return 2;
+            case "lpg": return 3;
+            default: return 0;
         }
     }
 
@@ -252,13 +189,15 @@ public class HomeActivity extends AppCompatActivity {
         handler.removeCallbacks(updateTimeRunnable);
     }
 
-    private void writeNotif(String message, long flame, double lpg, double suhu){
+    // Simpan riwayat ke Firebase
+    private void writeNotif(String message, long flame, double lpg, double suhu) {
         DatabaseReference notifRef = FirebaseDatabase.getInstance().getReference("notifikasi");
-        String notifKey = notifRef.push().getKey();
-        notifRef.child(notifKey).child("time").setValue(currentDateTime);
-        notifRef.child(notifKey).child("api").setValue(String.valueOf(flame));
-        notifRef.child(notifKey).child("suhu").setValue(String.valueOf(suhu));
-        notifRef.child(notifKey).child("asap").setValue(String.valueOf(lpg));
-        notifRef.child(notifKey).child("status").setValue(String.valueOf(message));
+        String key = notifRef.push().getKey();
+
+        notifRef.child(key).child("time").setValue(currentDateTime);
+        notifRef.child(key).child("api").setValue(String.valueOf(flame));
+        notifRef.child(key).child("suhu").setValue(String.valueOf(suhu));
+        notifRef.child(key).child("asap").setValue(String.valueOf(lpg));
+        notifRef.child(key).child("status").setValue(message);
     }
 }
--- app/src/main/java/com/example/firealert/NotificationHistoryActivity.java
+++ app/src/main/java/com/example/firealert/NotificationHistoryActivity.java
@@ -1,6 +1,5 @@
 package com.example.firealert;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,64 +24,57 @@ public class NotificationHistoryActivity extends AppCompatActivity {
     private RecyclerView recyclerViewNotifications;
     private Button buttonPantauanKondisi;
 
-    SensorAdapter sensorAdapter;
-
-    DatabaseReference notifReference;
-    FirebaseDatabase database;
-
-    List<NotificationItem> notificationList;
-    NotificationAdapter notifAdapter;
-
+    private DatabaseReference notifReference;
+    private List<NotificationItem> notificationList;
+    private NotificationAdapter notifAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_history);
 
-        database = FirebaseDatabase.getInstance();
-        notifReference = database.getReference("notifikasi");
+        // Firebase
+        notifReference = FirebaseDatabase.getInstance().getReference("notifikasi");
 
+        // Binding UI
         recyclerViewNotifications = findViewById(R.id.recyclerViewNotifications);
         buttonPantauanKondisi = findViewById(R.id.buttonPantauanKondisi);
 
-        // Dummy data for notifications
+        // List & Adapter
         notificationList = new ArrayList<>();
-//        notificationList.add(new NotificationItem("", "", "Tidak terdeteksi", "30°C", "80ppm"));
-//        notificationList.add(new NotificationItem("1 Juli 2024, 12:33 PM", "Terjadi Kebakaran", "Terdeteksi", "60°C", "400ppm"));
-//        notificationList.add(new NotificationItem("9 Juni 2024, 2:05 PM", "Tidak Kebakaran", "Tidak terdeteksi", "35°C", "100ppm"));
-//        notificationList.add(new NotificationItem("31 Januari 2024, 1:15 PM", "Terjadi Kebakaran", "Terdeteksi", "70°C", "450ppm"));
-//        notificationList.add(new NotificationItem("3 Agustus 2023, 3:15 PM", "Terjadi Kebakaran", "Terdeteksi", "55°C", "350ppm"));
-
-        // Set up RecyclerView
         notifAdapter = new NotificationAdapter(notificationList);
+
         recyclerViewNotifications.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotifications.setAdapter(notifAdapter);
 
-        // Button listener for navigating to the monitoring screen
-        buttonPantauanKondisi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               finish();
-            }
-        });
+        // Tombol kembali ke halaman sebelumnya
+        buttonPantauanKondisi.setOnClickListener(v -> finish());
 
-       notifReference.addValueEventListener(new ValueEventListener() {
+        // Load data dari Firebase
+        loadNotificationsFromFirebase();
+    }
+
+    private void loadNotificationsFromFirebase() {
+        notifReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 notificationList.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                    Log.d("SNAP", snapshot.toString());
-                    NotificationItem notifItem  = snapshot.getValue(NotificationItem.class);
-                    if (notifItem != null) {
-                        notificationList.add(notifItem);
+
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    NotificationItem item = data.getValue(NotificationItem.class);
+                    if (item != null) {
+                        notificationList.add(item);
                     }
                 }
+
                 notifAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(NotificationHistoryActivity.this, "Gagal memuat data notifikasi", Toast.LENGTH_SHORT).show();
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(NotificationHistoryActivity.this,
+                        "Gagal memuat data notifikasi",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
